371b3ac749b145dcfbdda4b4f58ed2c0e50a8478
weixiaobao93 <916198073@qq.com>
2017-10-09 20:31:32 +0800

    UP-jekyll

---
+++ assets/js/work.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/10/9.
+ */
+var r_top = function(tid,t_timer){
+    var Top = document.getElementById(tid);
+    $(Top).click(function(){
+        $('html,body').animate({'scrollTop':0},t_timer);
+    });
+    $(window).scroll(function () {
+        var winHeight = $(document).scrollTop();
+        var scrollY = $(document).scrollTop();
+        if( scrollY > 10){
+            $(Top).show(t_timer);
+        }else {
+            $(Top).hide(t_timer);
+        }
+    });
+};
