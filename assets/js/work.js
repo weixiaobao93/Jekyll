d81017a3536dcf30a859e5c6767a3f4f01d5f2e0
weixiaobao93 <916198073@qq.com>
2017-10-16 17:50:17 +0800

    UP20171016

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,18 +1,29 @@
 /**
- * Created by Administrator on 2017/10/9.
+ * Created by wxb on 2017/10/9.
  */
-var r_top = function(tid,t_timer){
-    var Top = document.getElementById(tid);
-    $(Top).click(function(){
+function navSF(ClassName,Ochild,Tchild,time) {
+    var obj_ClassName = document.getElementsByClassName(ClassName);
+    $(function () {
+        $(obj_ClassName).height(document.body.scrollHeight);
+        $(obj_ClassName).children(Tchild).hide();
+    })
+    $(obj_ClassName).children(Ochild).bind('click',function () {
+        $(this).css({background:'#bdd2d2'}).siblings(Ochild).css({background:'#e7e7e7'});
+        $(this).next(Tchild).slideToggle(time).siblings(Tchild).slideUp(time);
+    });
+}
+function rTop(tid,t_timer){
+    var obj_Top = document.getElementById(tid);
+    $(obj_Top).click(function(){
         $('html,body').animate({'scrollTop':0},t_timer);
     });
     $(window).scroll(function () {
         var winHeight = $(document).scrollTop();
         var scrollY = $(document).scrollTop();
         if( scrollY > 10){
-            $(Top).show(t_timer);
+            $(obj_Top).show(t_timer);
         }else {
-            $(Top).hide(t_timer);
+            $(obj_Top).hide(t_timer);
         }
     });
 };
